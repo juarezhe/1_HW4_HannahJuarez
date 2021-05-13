84ed2e1de80a10ffc77fa1299deaf663f326f4e4
Hannah Juarez <23268165+juarezhe@users.noreply.github.com>
2021-05-13 15:06:10 -0700

    Add Back button element plus functionality to APIActivity.kt, PermissionActivity.kt, and LibraryActivity.kt.

--- app/src/main/java/com/example/a1_hw4_hannahjuarez/APIActivity.kt
+++ app/src/main/java/com/example/a1_hw4_hannahjuarez/APIActivity.kt
@@ -1,11 +1,17 @@
 package com.example.a1_hw4_hannahjuarez
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class APIActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_api)
+
+        findViewById<Button>(R.id.button_api_back).setOnClickListener {
+            this.finish()
+        }
     }
 }
--- app/src/main/java/com/example/a1_hw4_hannahjuarez/PermissionActivity.kt
+++ app/src/main/java/com/example/a1_hw4_hannahjuarez/PermissionActivity.kt
@@ -2,10 +2,15 @@ package com.example.a1_hw4_hannahjuarez
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class PermissionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_permission)
+
+        findViewById<Button>(R.id.button_permission_back).setOnClickListener {
+            this.finish()
+        }
     }
 }
