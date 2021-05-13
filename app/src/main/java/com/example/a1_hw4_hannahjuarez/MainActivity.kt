ca082a32352205b76db43c3fd2eba177c9dc7f9d
Hannah Juarez <23268165+juarezhe@users.noreply.github.com>
2021-05-13 15:01:13 -0700

    Add functionality to buttons in activity_main.xml

--- app/src/main/java/com/example/a1_hw4_hannahjuarez/MainActivity.kt
+++ app/src/main/java/com/example/a1_hw4_hannahjuarez/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.a1_hw4_hannahjuarez
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button_api).setOnClickListener {
+            startActivity(
+                Intent(this, APIActivity::class.java)
+            )
+        }
+        findViewById<Button>(R.id.button_library).setOnClickListener {
+            startActivity(
+                Intent(this, PermissionActivity::class.java)
+            )
+        }
+        findViewById<Button>(R.id.button_permission).setOnClickListener {
+            startActivity(
+                Intent(this, LibraryActivity::class.java)
+            )
+        }
     }
 }
