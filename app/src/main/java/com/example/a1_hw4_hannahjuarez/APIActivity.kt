b8c646eaced905da5bc6430a1cb92122df811128
Hannah Juarez <23268165+juarezhe@users.noreply.github.com>
2021-05-13 14:46:50 -0700

    Create activities and layouts. Add buttons to activity_main.xml

---
+++ app/src/main/java/com/example/a1_hw4_hannahjuarez/APIActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a1_hw4_hannahjuarez
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class APIActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_api)
+    }
+}
